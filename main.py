61cbd6e5ec72a2323a96143b8659380cfbe3535e
Sander Holmen <sanderholmen96@gmail.com>
2022-01-10 10:46:00 +0100

    added score points and some tweaks

--- main.py
+++ main.py
@@ -17,6 +17,38 @@ p2_speed = 0
 #create ball object
 game_ball = pygame.Rect(500, 350, 10, 10)
 
+#defining borders
+screen_right = 1000
+screen_left = 0
+screen_top = 0
+screen_bottom = 700
+
+# score
+p1_points = 0
+p2_points = 0
+
+# ball speed
+x_speed = -3
+y_speed = -3
+
+def bounce_ball():
+    global screen_top, screen_bottom, y_speed, x_speed
+
+    if game_ball.top <= screen_top or game_ball.bottom >= screen_bottom:
+        y_speed *= -1
+
+    if game_ball.colliderect(p1_rect) or game_ball.colliderect(p2_rect):
+        x_speed *= -1
+
+def score():
+    global screen_right, screen_left, y_speed, x_speed, p1_points, p2_points
+
+    if game_ball.left <= screen_left:
+        p1_points += 1
+
+    if game_ball.right >= screen_right:
+        p2_points += 1
+
 
 
 while True:
@@ -35,7 +67,6 @@ while True:
                 p2_speed = -6
             if event.key == pygame.K_s:
                 p2_speed = 6
-
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
                 p1_speed = 0
@@ -46,9 +77,28 @@ while True:
             if event.key == pygame.K_s:
                 p2_speed = 0
 
+    bounce_ball()
+
+    score()
+
     # add movement to players
     p1_rect.y += p1_speed
     p2_rect.y += p2_speed
+
+    #checking if players hit borders
+    if p1_rect.y <= 0:
+        p1_rect.y = 0
+    if p1_rect.y >= 600:
+        p1_rect.y = 600
+    if p2_rect.y <= 0:
+        p2_rect.y = 0
+    if p2_rect.y >= 600:
+        p2_rect.y = 600
+
+    # game ball movement
+    game_ball.x += x_speed
+    game_ball.y += y_speed
+
 
     screen.fill((30, 30, 30))
 
